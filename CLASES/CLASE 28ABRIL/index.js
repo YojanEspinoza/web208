09d4f66575aae11f13bab223225a1c06b09bd194
YojanEspinoza <yojanespinoza18@aragon.unam.mx>
2025-04-28 10:40:27 -0600

    uso de node y http

---
+++ CLASES/CLASE 28ABRIL/index.js
@@ -0,0 +1,19 @@
+//importar el modulo de http que viene incluido en node
+const serverCreadoHTTP = require('http');
+
+//Definimos el uerto donde va atener salida nuestro servidor
+
+const puertoSalida = 3008;
+
+//Crear servidor
+
+const servidorCreado = serverCreadoHTTP.createServer((req, res)=>{
+    //Establecer el encabezado de respuesta 
+
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hola mundo desde Node.js con el modulo http');
+});
+
+servidorCreado.listen(puertoSalida,()=>{
+    console.log(`Servidor en escucha desde: http://localhost:${puertoSalida}`);
+});
