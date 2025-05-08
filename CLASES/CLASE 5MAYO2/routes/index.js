2f9077bc6c83d7554ccfa3fa41c613017dac7a81
YojanEspinoza <yojanespinoza18@aragon.unam.mx>
2025-05-07 22:18:22 -0600

    Uso de rutas

--- CLASES/CLASE 5MAYO2/routes/index.js
+++ CLASES/CLASE 5MAYO2/routes/index.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const path = require("path");
 
 //Definicion de las rutas multiples
-
+//Ruta inicial
 router.get("/",(req, res)=>{
     res.sendFile(path.join(__dirname,"../index.html"));
 });
-
+//Ruta hacia un Formulario
 router.get("/registro",(req, res)=>{
     res.sendFile(path.join(__dirname,"../registro.html"));
 });
-
+//Ruta hacia el formulario de la API
 router.get("/api",(req, res)=>{
     res.sendFile(path.join(__dirname,"../api.html"));
 });
-
+//Ruta hacia una calculadora
 router.get("/calculadora",(req, res)=>{
     res.sendFile(path.join(__dirname,"../calculadora.html"));
 });
-
+//Ruta hacia un esquema elaborado con DIVS
 router.get("/esquema",(req, res)=>{
     res.sendFile(path.join(__dirname,"../esquema.html"));
 });
